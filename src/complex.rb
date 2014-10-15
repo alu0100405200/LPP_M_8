2a28cc70f4cd14c92b352ec242fc12c1e9b07941
Javier Luis Moreno Villena <alu0100405200@ull.edu.es>
2014-10-15 12:34:34 +0100

    Esqueleto de la clase Complex

--- src/complex.rb
+++ src/complex.rb
@@ -1,1 +1,32 @@
+class Complex
 
+  attr_reader :a, :b
+
+  def initialize
+    @a, @b = 0, 0
+  end
+
+  def initialize(a, b)
+    @a, @b = a, b
+  end
+
+  def to_s
+    "#{@a} + #{@b}i"
+  end
+
+  def * (value)
+  end
+
+  def * (other)
+  end
+
+  def -@
+  end
+
+  def - (other)
+  end
+
+  def + (other)
+  end
+
+end
