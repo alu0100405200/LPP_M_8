d43b5a5656dedce827daad7cb57f34ee034cb38c
Javier Luis Moreno Villena <alu0100405200@ull.edu.es>
2014-10-15 12:57:44 +0100

    Metodos corregidos

--- src/complex.rb
+++ src/complex.rb
@@ -11,12 +11,17 @@ class Complejo
   end
 
   def * (value)
+    Complejo.new(@a * value, @b * value)
   end
 
   def -@
+    Complejo.new(@a * -1, @b * -1)
   end
 
   def mult (other)
+    multA = (@a * other.a) - (@b * other.b)
+    multB = (@a * other.b) - (@b * other.a)
+    Complejo.new(multA,multB)
   end
 
   def + (other) 
