fead05b19026150b0ea847e5f4045425e61a3a6e
alu0100611519 <alu0100611519@ull.edu.es>
2014-10-17 09:18:40 +0000

    modificado el fichero tc_complex.rb

--- test/tc_complex.rb
+++ test/tc_complex.rb
@@ -1,20 +1,14 @@
-require 'LPP_M_8/src/complex.rb'
-require 'test/unit'
-
+require '~/LPP_M_8/src/complex'
+require 'test/unit' 
 
 class Testcomplejo < Test::Unit::TestCase
-
-	def setup
-	@c1 = Complejo.new(1,1)
-	@c2 = Complejo.new(2,2) 
-	end
-
-	def test_simple
-	assert_equal("1,1i" , @c1.to_s)
-	assert¡equal("2,2i",  @c2.to_s)
-        assert_equal("3,3i",  (@c1 + @c2).to_s)
-
-	end
-
-
+        def setup
+        @c1 = Complejo.new(1,1) 
+	@c2 = Complejo.new(2,2)
+        end
+        def test_simple
+        assert_equal("1,1i" , @c1.to_s) 
+	assert_equal("2,2i", @c2.to_s) 
+	assert_equal("3,3i", (@c1 + @c2).to_s)
+        end
 end
